db088a76d349a221151c78f7a3125ac277abcfd9
mcubic02 <mcubic02@fesb.hr>
2022-01-13 12:32:41 +0100

    dodana datoteka studenti.txt

---
+++ 1labovii/Project1/1zad.c
@@ -0,0 +1,103 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define MAX_SIZE 128
+#define MAX_LINE 1024
+
+typedef struct _student {
+    char ime[MAX_SIZE];
+    char prezime[MAX_SIZE];
+    double bodovi;
+}student;
+int ProcitajBrojRedakaIzDatoteke(char* nazivDatoteke);
+student* AlocirajMemorijuProcitajStudente(int brojStudenata, char* nazivDatoteke);
+void IspisStudenataIzDatoteke(student* studenti, int brojStudenata, double max);
+double MaxBrBd(int brojStudenata, student* studenti);
+int main()
+{
+    int brojStudenata;
+    double max;
+
+    student* studenti = NULL;
+    char* datoteka = "studenti.txt";
+    brojStudenata = ProcitajBrojRedakaIzDatoteke(datoteka);
+    studenti = AlocirajMemorijuProcitajStudente(brojStudenata, datoteka);
+    max = MaxBrBd(brojStudenata, studenti);
+    IspisStudenataIzDatoteke(studenti, brojStudenata, max);
+
+
+
+    return 0;
+}
+double MaxBrBd(int brojStudenata, student* studenti)
+{
+    int i, f = 0;
+    double max;
+    max = studenti[0].bodovi;
+    for (i = 1;i < brojStudenata;i++)
+    {
+        if (studenti[i].bodovi > max) max = studenti[i].bodovi;
+    }
+    return max;
+}
+
+int ProcitajBrojRedakaIzDatoteke(char* nazivDatoteke)
+{
+    int brojac = 0;
+    FILE* datoteka = NULL;
+    char buffer[MAX_LINE] = { 0 };
+
+    datoteka = fopen(nazivDatoteke, "r");
+    if (!datoteka)
+    {
+        printf("Greska!\n");
+        return -1;
+    }
+    while (!feof(datoteka))
+    {
+        fgets(buffer, MAX_LINE, datoteka);
+        brojac++;
+    }
+    fclose(datoteka);
+
+    return brojac;
+}
+
+student* AlocirajMemorijuProcitajStudente(int brojStudenata, char* nazivDatoteke)
+{
+    int brojac = 0;
+    FILE* datoteka = NULL;
+    student* studenti = NULL;
+
+    studenti = (student*)malloc(brojStudenata * sizeof(student));
+    datoteka = fopen(nazivDatoteke, "r");
+    if (!datoteka)
+    {
+        printf("Greska alociranja!\n");
+        free(studenti);
+        return NULL;
+    }
+    while (!feof(datoteka))
+    {
+
+        fscanf(datoteka, " %s %s %lf", studenti[brojac].ime, studenti[brojac].prezime, &studenti[brojac].bodovi);
+        brojac++;
+    }
+    fclose(datoteka);
+
+    return studenti;
+}
+void IspisStudenataIzDatoteke(student* studenti, int brojStudenata, double max)
+{
+    double relativno[100] = {0};
+    int brojac = 0;
+    while (brojac != brojStudenata)
+    {
+        relativno[brojac] = studenti[brojac].bodovi / max * 100;
+        printf("%s %s %.2lf %.2lf\n", studenti[brojac].ime, studenti[brojac].prezime, studenti[brojac].bodovi, relativno[brojac]);
+        brojac++;
+    }
+
+}
